54f6bd9ae696b67ae93a25e04ec599dd0a9792b4
Luke Reid <luke@reid.org.nz>
2025-11-04 16:04:49 +1300

    Update to version 2.2.5rc7 in plugins/screen-shot

--- dist/screen-shot.js
+++ dist/screen-shot.js
@@ -1,10 +1,10 @@
-const b = (s, o = {}, g = "") => {
-  const r = document.createElement(s);
-  for (let l in o)
-    r.setAttribute(l, o[l]);
-  return r.innerHTML = g, r;
-}, v = (s, o) => (document.getElementById(s) || document.getElementsByTagName("head")[0].prepend(b("STYLE", { type: "text/css" }, o)), !0), C = function(s = {}) {
-  let o, g, r, l, I, m;
+const b = (g, n = {}, r = "") => {
+  const a = document.createElement(g);
+  for (let l in n)
+    a.setAttribute(l, n[l]);
+  return a.innerHTML = r, a;
+}, v = (g, n) => (document.getElementById(g) || document.getElementsByTagName("head")[0].prepend(b("STYLE", { type: "text/css" }, n)), !0), C = function(g = {}) {
+  let n, r, a, l, I, m;
   v("geocam-screenshot-button", `
   .geocam-screenshot-button {
        background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgMjQgMjQiPjxwYXRoIGZpbGw9ImN1cnJlbnRDb2xvciIgZD0iTTQgNGgzbDItMmg2bDIgMmgzYTIgMiAwIDAgMSAyIDJ2MTJhMiAyIDAgMCAxLTIgMkg0YTIgMiAwIDAgMS0yLTJWNmEyIDIgMCAwIDEgMi0ybTggM2E1IDUgMCAwIDAtNSA1YTUgNSAwIDAgMCA1IDVhNSA1IDAgMCAwIDUtNWE1IDUgMCAwIDAtNS01bTAgMmEzIDMgMCAwIDEgMyAzYTMgMyAwIDAgMS0zIDNhMyAzIDAgMCAxLTMtM2EzIDMgMCAwIDEgMy0zIi8+PC9zdmc+');
@@ -13,82 +13,82 @@ const b = (s, o = {}, g = "") => {
        background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgMjQgMjQiPjxwYXRoIGZpbGw9ImN1cnJlbnRDb2xvciIgZD0iTTQgNWgzbDItMmg2bDIgMmgzYTIgMiAwIDAgMSAyIDJ2MTJhMiAyIDAgMCAxLTIgMkg0YTIgMiAwIDAgMS0yLTJWN2EyIDIgMCAwIDEgMi0ybTkuMDkgNC40NWwtMi4wNCAyLjczbDEuNTUgMi4wN2wtLjg3LjY2bC0yLjQ2LTMuMjdMNiAxNmgxMnoiLz48L3N2Zz4=');
     }
   `);
-  const p = (a) => {
+  const u = (c) => {
     if (I) {
-      const n = {
+      const o = {
         action: "screenshot",
         payload: {
-          img: a,
+          img: c,
           url: document.location.href
         }
       };
-      I.postMessage(n);
+      I.postMessage(o);
     }
-  }, u = function(a, n, t) {
+  }, p = function(c, o, e) {
     return new Promise((i) => {
-      const e = new Image();
-      e.onload = function() {
-        const c = document.createElement("canvas"), d = c.getContext("2d");
-        c.width = Math.min(n, e.width), c.height = n * e.height / e.width, d.drawImage(e, 0, 0, c.width, c.height), c.toBlob(i, a.type, t);
-      }, e.src = URL.createObjectURL(a);
+      const t = new Image();
+      t.onload = function() {
+        const s = document.createElement("canvas"), d = s.getContext("2d");
+        s.width = Math.min(o, t.width), s.height = o * t.height / t.width, d.drawImage(t, 0, 0, s.width, s.height), s.toBlob(i, c.type, e);
+      }, t.src = URL.createObjectURL(c);
     });
-  }, A = async (a) => {
-    const n = o.renderer.domElement;
-    n.style.filter = "invert(1)";
-    let t = await new Promise((e) => n.toBlob(e));
-    m && (t = await u(t, m, 0.9));
-    const i = [new ClipboardItem({ [t.type]: t })];
-    await navigator.clipboard.write(i), p(t), n.style.removeProperty("filter");
-  }, y = async (a) => {
+  }, A = async (c) => {
+    const o = n.renderer.domElement;
+    o.style.filter = "invert(1)";
+    let e = await new Promise((t) => o.toBlob(t));
+    m && (e = await p(e, m, 0.9));
+    const i = [new ClipboardItem({ [e.type]: e })];
+    await navigator.clipboard.write(i), u(e), o.style.removeProperty("filter");
+  }, y = async (c) => {
     document.body.style.filter = "invert(1)";
-    const n = o.renderer.domElement, t = n.getBoundingClientRect(), i = l.container.getBoundingClientRect(), e = {
-      top: Math.min(t.top, i.top),
-      left: Math.min(t.left, i.left),
-      bottom: Math.max(t.bottom, i.bottom),
-      right: Math.max(t.right, i.right)
+    const o = n.renderer.domElement, e = o.getBoundingClientRect(), i = l.container.getBoundingClientRect(), t = {
+      top: Math.min(e.top, i.top),
+      left: Math.min(e.left, i.left),
+      bottom: Math.max(e.bottom, i.bottom),
+      right: Math.max(e.right, i.right)
     };
-    e.width = e.right - e.left, e.height = e.bottom - e.top;
-    const c = document.createElement("canvas");
-    c.width = e.width, c.height = e.height;
-    const d = c.getContext("2d"), D = await l.takeScreenshot(), w = new Image();
+    t.width = t.right - t.left, t.height = t.bottom - t.top;
+    const s = document.createElement("canvas");
+    s.width = t.width, s.height = t.height;
+    const d = s.getContext("2d"), D = await l.takeScreenshot(), w = new Image();
     w.src = D.dataUrl, await new Promise((M) => {
       w.onload = M;
-    }), t.witdh > i.width || t.height > i.height ? (d.drawImage(
-      n,
-      t.left - e.left,
-      t.top - e.top
+    }), e.witdh > i.width || e.height > i.height ? (d.drawImage(
+      o,
+      e.left - t.left,
+      e.top - t.top
     ), d.drawImage(
       w,
-      i.left - e.left,
-      i.top - e.top
+      i.left - t.left,
+      i.top - t.top
     )) : (d.drawImage(
       w,
-      i.left - e.left,
-      i.top - e.top
+      i.left - t.left,
+      i.top - t.top
     ), d.drawImage(
-      n,
-      t.left - e.left,
-      t.top - e.top
+      o,
+      e.left - t.left,
+      e.top - t.top
     ));
-    let h = await new Promise((M) => c.toBlob(M));
-    m && (h = await u(h, m, 0.9));
+    let h = await new Promise((M) => s.toBlob(M));
+    m && (h = await p(h, m, 0.9));
     const f = [new ClipboardItem({ [h.type]: h })];
-    await navigator.clipboard.write(f), p(h), document.body.style.removeProperty("filter");
+    await navigator.clipboard.write(f), u(h), document.body.style.removeProperty("filter");
   };
-  this.init = function(a) {
-    o = a, g = b("DIV", {
+  this.init = function(c) {
+    n = c, r = b("DIV", {
       class: "geocam-screenshot-button geocam-viewer-control-button",
       title: "Copy a screenshot of the current viewer image to the clipboard"
-    }), o.addControl(g, "left-top"), g.addEventListener("click", A, !1);
-    const n = s.channel;
-    n && "BroadcastChannel" in window && (I = new BroadcastChannel(n), console.log("broadcasting on channel", n)), m = parseInt(s.maxWidth);
-  }, this.arcgisView = function(a) {
-    console.log("screen shot add view", a), l || (l = a, r = b("DIV", {
+    }), n.addControl(r, "left-top"), r.addEventListener("click", A, !1);
+    const o = g.channel;
+    o && "BroadcastChannel" in window && (I = new BroadcastChannel(o), console.log("broadcasting on channel", o)), m = parseInt(g.maxWidth);
+  }, this.arcgisView = function(c) {
+    console.log("screen shot add view", c), l || (l = c, a = b("DIV", {
       class: "geocam-viewshot-button geocam-viewer-control-button",
       title: "Copy a screenshot of the current viewer image and map to the clipboard"
-    }), o.addControl(r, "left-top", { after: g }), r.addEventListener("click", y, !1));
+    }), n.addControl(a, "left-top", { after: r }), a.addEventListener("click", y, !1));
   }, this.destroy = function() {
-    r && o.wrapper.removeChild(r), o.wrapper.removeChild(g);
+    a && n.wrapper.removeChild(a), n.wrapper.removeChild(r);
   };
 };
 class T extends HTMLElement {
@@ -97,8 +97,19 @@ class T extends HTMLElement {
   }
   connectedCallback() {
     console.log("screen-shot connected");
-    const o = this.parentNode;
-    o.viewer && o.viewer.plugin ? (this.plugin = new C({ channel: this.getAttribute("channel"), maxWidth: this.getAttribute("max-width") }), o.viewer.plugin(this.plugin)) : console.error("GeocamViewerScreenShot must be a child of GeocamViewer");
+    const n = this.closest("geocam-viewer");
+    if (!n) {
+      console.error("GeocamViewerScreenShot must be a child of GeocamViewer");
+      return;
+    }
+    const r = () => {
+      const a = n.viewer;
+      a && typeof a.plugin == "function" ? (this.plugin = new C({
+        channel: this.getAttribute("channel"),
+        maxWidth: this.getAttribute("max-width")
+      }), a.plugin(this.plugin)) : setTimeout(r, 50);
+    };
+    r();
   }
   disconnectedCallback() {
     this.plugin = null, console.log("screen-shot disconnected");
